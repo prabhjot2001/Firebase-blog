b694f983030ed938ce8a0e10a38b21fc12714653
prabhjot2001 <lazycoder001@gmail.com>
2023-12-16 14:31:28 +0530

    initialization complete

---
+++ src/fireBase/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import {getStorage} from 'firebase/storage'
+import {getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "blog-rush.firebaseapp.com",
+  projectId: "blog-rush",
+  storageBucket: "blog-rush.appspot.com",
+  messagingSenderId: "525464607179",
+  appId: "1:525464607179:web:79acad90593ae58f75fe4e",
+  measurementId: "G-DT6KH9Y360"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const authentication = getAuth();
+export const authProvider = new GoogleAuthProvider();
+export const storage = getStorage();
+export const db = getFirestore(app);
